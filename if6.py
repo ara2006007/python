c6847c0bd7c1c3701850b8884b6639037b6e97eb
Selvakumar Aradanan <aradanan18@gmail.com>
2026-02-11 09:32:05 +0530

    Update if6.py

--- if6.py
+++ if6.py
@@ -1,1 +1,8 @@
-num=int(input("Please input the number you want to select"))
+num=int(input("Please input the number you want to check "))
+if num>0 :
+    if (num%2==0) :
+        print("Even number")
+    else :
+        print("odd number")
+else :
+    print("Not a positve number")
